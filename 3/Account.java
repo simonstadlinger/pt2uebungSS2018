49e776fa7845600a4d7e75a2915aa19f7b37a2f2
Jonas Kordt <jonas.kordt@student.hpi.de>
2018-05-14 19:09:42 +0200

    first template for Account

---
+++ 3/Account.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+public class Account {
+
+	public String login_name;
+	public String password;
+	public int uid;
+	public int gid;
+	public String user_name;
+	public String directory;
+	public String shell;
+
+	public Account(String login_name, String password, int uid, int gid, String user_name, String directory, String shell) {
+		this.login_name = login_name;
+		this.password   = password;
+		this.uid        = uid;
+		this.gid        = gid;
+		this.user_name  = user_name;
+		this.directory  = directory;
+		this.shell      = shell;
+	}
+
+	public static void main(String args[]){
+		open("passwd.txt");
+	}
+
+	public static Account[] open(String filename) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filename));	
+
+			String accInfo = reader.readLine();
+			System.out.println(accInfo);
+
+			return null;
+		}
+		catch(IOException e) {
+  			System.out.println("ERROR: File 'passwd' wasn't found!\n");
+  			return null;
+		}
+		
+	}
+
+	public static Account find_account(Account[] liste, String login_name) {
+		Account result = new Account("a", "b", 1, 2, "d", "e", "f");
+		return result;
+	}
+}
