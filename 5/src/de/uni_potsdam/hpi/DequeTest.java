bc4fe54d30b2e61a4a664b512f4dd94c4cc18195
simonstadlinger <stadlinger.simon@gmail.com>
2018-06-19 19:00:14 +0200

    added some tests

--- 5/src/de/uni_potsdam/hpi/DequeTest.java
+++ 5/src/de/uni_potsdam/hpi/DequeTest.java
@@ -16,23 +16,43 @@ public class DequeTest {
 	public void emptyDequeInsert() {
         Object o = new Object();
         q.addLast(o);
-        assertEquals("inserted and removed Objects are not the same", q.removeLast(), o);
+        assertEquals("inserted and removed Objects are not the same - empty Deque", q.removeFirst(), o);
 	}
 
 	@Test
-	public void testB() {
+	public void sevenElementsDequeInsertLast() {
 		//Fügt man in eine Deque mit 7 Elementen eines mit addLast ein, erhält man es mit removeLast sofort zurück.
+        Object o = new Object();
+        q7.addLast(o);
+        assertEquals("inserted last and removed Objects last are not the same - seven element Deque", q7.remoteLast(), o);
 	}
-
 	@Test
-	public void testC() {
+	public void sevenElementsDequeInsertFirst() {
 		//Fügt man in eine Deque mit 7 Elementen eines mit addFirst ein, erhält man es mit removeFirst sofort zurück.
-	}
+        Object o = new Object();
+        q7.addFirst(o);
+        assertEquals("inserted fist and removed first Objects are not the same - seven element Deque", q7.remoteFirst(), o);
+    }
+
 
 	@Test
-	public void testD() {
+	public void insert6nullremote6null() {
 		//Fügt man in eine Deque 6 mal den Wert null ein und danach ein von null verschiedenes Objekt o , und führt danach wiederholt removeFirst aus, so erhält man 6 mal den Wert null , und danach o .
+        for(int i = 0; i<6;i++)
+        {
+            q.addLast(null);
+        }
+        Object o = new Object();
+        q.addLast(o);
+        int null_count=0;
+        int Object_count=0;
+        for(int i = 0; i<6;i++)
+        {
+           assertEquals("object not null", q.removeFirst(), o); 
+        }
+        asserEquals("objects not o", q.removeFirst(), o);
 	}
+
 
 	@Test
 	public void testE() {
