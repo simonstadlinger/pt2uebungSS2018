6d0de4f7f15dc7966a90999cc062abbad1ce7762
Jonas Kordt <jonas.kordt@student.hpi.de>
2018-06-19 18:38:38 +0200

    implemented ArrayDeque

--- 5/src/de/uni_potsdam/hpi/ArrayDeque.java
+++ 5/src/de/uni_potsdam/hpi/ArrayDeque.java
@@ -14,10 +14,14 @@ public class ArrayDeque implements Deque {
 	private int length;
 	private Object[] deque;
 
+	private int first;
+	private int last;
+
 	public ArrayDeque(int c) {
 		cap = c;
 		length = 0;
 		deque = new Object[c];
+
 	}
 
 	public int capacity() {
@@ -28,5 +32,61 @@ public class ArrayDeque implements Deque {
 		return length;
 	}
 
+	public void clear() {
+		length = 0;
+		deque = [];
+	}
+
+	public void addFirst(Object e) throws DequeFull {
+		if (length < cap) {
+			if (length != 0) {
+				first = (first - 1) % cap;
+			}
+			deque[first] = e;
+			length++;
+		} else {
+			throw new DequeFull("Deque is full!");
+		}
+	}
+
+	public void addLast(Object e) throws DequeFull {
+		if (length < cap) {
+			if (length != 0) {
+				last = (last + 1) % cap;
+			}
+			deque[last] = e;
+			length++;
+		} else {
+			throw new DequeFull("Deque is full!");
+		}
+	}
+ 
+	public Object removeFirst() throws DequeEmpty {
+		if (length > 0) {
+			Object result = deque[first];
+			if (first != last) {
+				first = (first + 1) % cap;
+			}
+			length--;
+			return result;
+		} else {
+			throw new DequeEmpty("Deque is empty!");
+			return null;
+		}
+	}
+ 
+	public Object removeLast() throws DequeEmpty {
+		if (length > 0) {
+			Object result = deque[last];
+			if (last =! first) {
+				last = (last - 1) % cap;
+			}
+			length--;
+			return result;
+		} else {
+			throw new DequeEmpty("Deque is empty!");
+			return null;
+		}
+	}
 
 }
