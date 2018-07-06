22dc80384bffa7b0f491b4bae97cc874c7cd2a9c
simonstadlinger <stadlinger.simon@gmail.com>
2018-07-06 20:46:23 +0200

    added sortrandom

--- 6/src/SortRandom.java
+++ 6/src/SortRandom.java
@@ -4,8 +4,8 @@ import java.util.SplittableRandom;
 public class SortRandom{
     public static void main(String[] args){
         StableQuickSortableList list = new StableQuickSortableList();
-        int j = new SplittableRandom().nextInt(20, 50);
-        int[] random = new SplittableRandom().ints(j, 0, 100).parallel().toArray();
+        int j = new SplittableRandom().nextInt(0, 10);
+        int[] random = new SplittableRandom().ints(j, 0, 20).parallel().toArray();
 
         for(int i: random){
             System.out.println(i);
@@ -13,7 +13,8 @@ public class SortRandom{
             list.addLast(val);
         }
 
-        //list.sort();
+        list.print();
+        list.sort();
         list.print();
     }
 }
